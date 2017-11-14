5c4ac41b23cbe9fbd3669acfd3b9ebad0660508a
Ville <Ville@MSI.wlan.metropolia.fi>
2017-11-14 12:53:01 +0200

    sp service

--- buukkeriAPI/src/main/java/service/SPService.java
+++ buukkeriAPI/src/main/java/service/SPService.java
@@ -1,8 +1,18 @@
 package service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import dao.SPDAO_IF;
+import entity.SP_IF;
 
 @Service
 public class SPService {
-
+	
+	@Autowired
+	private SPDAO_IF spdao;
+	
+	public SP_IF[] readsps() {
+		return spdao.readSPs();
+	}
 }
