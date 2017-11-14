4eade81b36bf787f835009ceb623588b35ada74a
Ville <Ville@MSI.wlan.metropolia.fi>
2017-11-14 12:21:35 +0200

    service update

--- buukkeriAPI/src/main/java/service/UserService.java
+++ buukkeriAPI/src/main/java/service/UserService.java
@@ -1,5 +1,22 @@
 package service;
 
-public class UserService {
+import dao.UserDAO_IF;
+import entity.User_IF;
 
+public class UserService {
+	
+	private UserDAO_IF userdao;
+	
+ public User_IF getUser(String email, String pass) { 
+	 return userdao.readUser(email, pass);
+ }
+ public boolean createUser(User_IF user) {
+	 return userdao.createUser(user);
+ }
+ public boolean updateUser(User_IF user) {
+	 return userdao.updateUser(user);
+ }
+ public boolean deleteUser(User_IF user) {
+	 return userdao.deleteUser(user);
+ }
 }
