16483a2a41111d19e2429fd58b94cf08ea956d92
romppane <ronim@metropolia.fi>
2017-11-14 12:35:04 +0200

    main add

---
+++ buukkeriAPI/src/main/java/main/springLauncher.java
@@ -0,0 +1,15 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springLauncher {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(springLauncher.class, args);
+
+	}
+
+}
