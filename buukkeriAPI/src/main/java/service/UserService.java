850efac1e18d305ef45209dce7d1374739025cb2
romppane <ronim@metropolia.fi>
2017-11-14 12:31:27 +0200

    annotations

--- buukkeriAPI/src/main/java/service/UserService.java
+++ buukkeriAPI/src/main/java/service/UserService.java
@@ -3,20 +3,23 @@ package service;
 import dao.UserDAO_IF;
 import entity.User_IF;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
 public class UserService {
-	
+	@Autowired
 	private UserDAO_IF userdao;
 	
  public User_IF getUser(String email, String pass) { 
-	 return userdao.readUser(email, pass);
+	 return this.userdao.readUser(email, pass);
  }
  public boolean createUser(User_IF user) {
-	 return userdao.createUser(user);
+	 return this.userdao.createUser(user);
  }
  public boolean updateUser(User_IF user) {
-	 return userdao.updateUser(user);
+	 return this.userdao.updateUser(user);
  }
  public boolean deleteUser(User_IF user) {
-	 return userdao.deleteUser(user);
+	 return this.userdao.deleteUser(user);
  }
 }
