8ed52e3a74ad25736f2210046b1aea4d4ba39a0a
Ville <Ville@192.168.1.35>
2017-11-16 16:20:03 +0200

    Service provider toimii restapina

--- buukkeriAPI/src/main/java/main/controller/SPController.java
+++ buukkeriAPI/src/main/java/main/controller/SPController.java
@@ -1,10 +1,13 @@
 package main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import main.entity.SP;
 import main.entity.SP_IF;
 import main.service.SPService;
 
@@ -17,5 +20,17 @@ public class SPController {
 	@RequestMapping(method = RequestMethod.GET)
 	public SP_IF[] readSPs() {
 		return spservice.readsps();
+	}
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public boolean createsp(@RequestBody SP sp) {
+		return spservice.createsp(sp);
+	}
+	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public boolean updatesp(@RequestBody SP sp) {
+		return spservice.updatesp(sp);
+	}
+	@RequestMapping(method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public boolean deletesp(@RequestBody SP sp) {
+		return spservice.deletesp(sp);
 	}
 }
--- buukkeriAPI/src/main/java/main/service/SPService.java
+++ buukkeriAPI/src/main/java/main/service/SPService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import main.dao.SPDAO_IF;
+import main.entity.SP;
 import main.entity.SP_IF;
 
 @Service
@@ -14,5 +15,14 @@ public class SPService {
 	
 	public SP_IF[] readsps() {
 		return spdao.readSPs();
+	}
+	public boolean createsp(SP_IF sp) {
+		return spdao.createSP(sp);
+	}
+	public boolean updatesp(SP_IF sp) {
+		return spdao.updateSP(sp);
+	}
+	public boolean deletesp(SP_IF sp) {
+		return spdao.deleteSP(sp);
 	}
 }
