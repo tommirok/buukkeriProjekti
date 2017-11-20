ef8c4dd0f3d94f947b0ede328a9ffc45ec1a8701
Ville <Ville@192.168.1.33>
2017-11-20 12:02:19 +0200

    user Controller

--- buukkeriAPI/src/main/java/main/controller/UserController.java
+++ buukkeriAPI/src/main/java/main/controller/UserController.java
@@ -1,10 +1,14 @@
 package main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import main.entity.User;
 import main.entity.User_IF;
 import main.service.UserService;
 
@@ -15,11 +19,22 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public User_IF getUser(String email, String pass) {
+	@RequestMapping(value ="/{email}&{pass}" ,method = RequestMethod.GET)
+	public User_IF getUser(@PathVariable("email")String email,@PathVariable("pass") String pass) {
 		return userService.getUser(email, pass);
 	}
-	
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public boolean createuser(@RequestBody User user) {
+		return userService.createUser(user);
+	}
+	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public boolean userupdate(@RequestBody User user) {
+		return userService.updateUser(user);
+	}
+	@RequestMapping(method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public boolean userdelete(@RequestBody User user) {
+		return userService.deleteUser(user);
+	}
 	
 	
 
