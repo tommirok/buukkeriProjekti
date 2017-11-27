577be3445d7bf838e25da174c120b492d7ee6a55
Ville <Ville@MSI.wlan.metropolia.fi>
2017-11-27 13:59:00 +0200

    shift service ja controller

--- buukkeriAPI/src/main/java/main/controller/ShiftController.java
+++ buukkeriAPI/src/main/java/main/controller/ShiftController.java
@@ -1,8 +1,42 @@
 package main.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import main.entity.Shift;
+import main.entity.Shift_IF;
+import main.service.ShiftService;
+
 @RestController
+@RequestMapping("/shifts")
 public class ShiftController {
 
+	@Autowired
+	private ShiftService service;
+	
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public boolean createshift(@RequestBody Shift shift) {
+		return service.createshift(shift);
+	}
+	@RequestMapping(method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public boolean deleteshift(@RequestBody Shift shift) {
+		return service.deleteshift(shift);
+	}
+	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public boolean updateshift(@RequestBody Shift shift) {
+		return service.updateshift(shift);
+	}
+	@RequestMapping(value ="/id={id}" ,method = RequestMethod.GET)
+	public Shift_IF getShiftByID(@PathVariable("id") int id) {
+		return service.getShiftByID(id);
+	}
+	@RequestMapping(value ="/actid={id}" ,method = RequestMethod.GET)
+	public Shift_IF[] getShiftByActID(@PathVariable("id") int id) {
+		return service.getShiftByActID(id);
+	}
 }
--- buukkeriAPI/src/main/java/main/service/ShiftService.java
+++ buukkeriAPI/src/main/java/main/service/ShiftService.java
@@ -1,8 +1,36 @@
 package main.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import main.dao.ShiftDAO;
+import main.entity.Shift;
+import main.entity.Shift_IF;
 
 @Service
 public class ShiftService {
 
+	@Autowired
+	private ShiftDAO shiftdao;
+
+	public boolean createshift(Shift shift) {
+		return shiftdao.createShift(shift);
+	}
+
+	public boolean deleteshift(Shift shift) {
+		return shiftdao.deleteShift(shift);
+	}
+
+	public boolean updateshift(Shift shift) {
+		return shiftdao.updateShift(shift);
+	}
+
+	public Shift_IF getShiftByID(int id) {
+		return shiftdao.readShiftById(id);
+	}
+
+	public Shift_IF[] getShiftByActID(int id) {
+		return shiftdao.readActivityShifts(id);
+	}
+	
 }
