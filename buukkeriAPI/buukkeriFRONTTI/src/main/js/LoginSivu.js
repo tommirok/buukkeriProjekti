7a2b780b17fe50db54699f0c4425b926bdea1554
Ville <Ville@10.112.206.145>
2017-12-07 11:49:59 +0200

    Login toiminnon alkua

--- buukkeriAPI/buukkeriFRONTTI/src/main/js/LoginSivu.js
+++ buukkeriAPI/buukkeriFRONTTI/src/main/js/LoginSivu.js
@@ -9,7 +9,7 @@ import {
 	  Link,
 		Switch
 	} from 'react-router-dom';
-	
+
 
 export default class Login extends React.Component{
   constructor(props){
@@ -20,26 +20,36 @@ export default class Login extends React.Component{
     };
     this.handleEmail = this.handleEmail.bind(this);
     this.handlePass = this.handlePass.bind(this);
+		this.handleLogin =this.handleLogin.bind(this);
   }
   handleEmail(value){
     this.setState({email: value})
-    console.log(value)
   }
   handlePass(value){
     this.setState({pass: value})
-    console.log(value)
   }
+	handleLogin(){
+		let user = callBookker("users/"+this.state.email+"&"+this.state.pass).then((data)=>{
+			console.log(data);
+			if(data!=""){
+				data = JSON.parse(data);
+				console.log(data);
+			}
+
+
+		});
+	}
   render(){
     return(
     <div>
       <ul className="list-group">
       <Input label="Nimi" type="text" onChange={this.handleEmail} />
       <Input label="Salasana:" type="password" onChange={this.handlePass} />
-      <li className="list-group-item"><button className="btn btn-success">Kirjaudu</button>  </li>
-      <li className="list-group-item"><Link to="/assets/Registration"><button className="btn btn-primary">Rekisteröidy</button></Link>  </li>
+      <li className="list-group-item"><button className="btn btn-success" onClick={this.handleLogin}>Kirjaudu</button>  </li>
+      <li className="list-group-item"><Link className="btn btn-primary" to="/assets/Registration">Rekisteröidy</Link>  </li>
       </ul>
-      
-      
+
+
     </div>
     );
   }
